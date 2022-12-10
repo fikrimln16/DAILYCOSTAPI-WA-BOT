75da96ea171cf32cf5ee10bf95ed45e9c5d8e1ea
Fikri Maulana <maulanafikri455@gmail.com>
2022-12-10 08:38:41 +0700

    dailycost api

--- main.py
+++ main.py
@@ -58,14 +58,14 @@ def sms_reply():
             db = get_db()
             row = db.execute("SELECT nama, jumlah, tanggal, pembayaran FROM pengeluaran WHERE tanggal BETWEEN '{} 00:00:00' AND '{} 23:59:59'".format(tanggal, tanggal)).fetchall()
             pesan = "Berikut adalah barang-barang yang anda beli pada tanggal {}\n\n"\
-                "------------------------------".format(tanggal)
+                "\n\n".format(tanggal)
             message.body(pesan)
             for i in row:
-                reply = "#===============#"\
-                            "nama barang : {} \n"\
+                reply = "nama barang : {} \n"\
                                 "harga barang : {} \n"\
                                     "tanggal beli : {} \n"\
-                                        "pembayaran : {}\n\n".format(i[0], i[1], i[2], i[3])
+                                        "pembayaran : {}\n\n"\
+                                            "\n".format(i[0], i[1], i[2], i[3])
                 message.body(reply)
                 responded = True
 
